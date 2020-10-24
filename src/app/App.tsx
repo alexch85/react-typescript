ddb3a6b4f67e31f52b4468577c8989fe6a946135
Alex <alexch85@windowslive.com>
2020-10-24 13:17:43 +0300

    App structure modified, sass installed

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Styles.module.scss';
+
+const App: React.FC = () => {
+	return (
+		<div className={styles.app}>
+			<h1>Welcome to react app</h1>
+		</div>
+	);
+};
+
+export default App;
