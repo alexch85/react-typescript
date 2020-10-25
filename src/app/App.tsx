923140c5466cf7e20c52b7c9bffff1f1be4d1e2c
Alex <alexch85@windowslive.com>
2020-10-25 18:02:09 +0200

    components and styles modified, interfaces.ts added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,43 @@
+import { ITabUserOptions } from '@testing-library/user-event';
 import React, { useState } from 'react';
 import Navbar from '../components/navbar/Navbar';
 import TodoForm from '../components/todoForm/TodoForm';
+import TodoList from '../components/todosList/TodoList';
+import { ITodo } from '../interfaces';
 import styles from './Styles.module.scss';
 
 const App: React.FC = () => {
-	const [todos, setTodos] = useState([]);
+	const [todos, setTodos] = useState<ITodo[]>([]);
 
 	const addTodoHandler = (title: string) => {
-		console.log('Add new Todo', title);
+		const newTodo = {
+			title,
+			id: Date.now(),
+			completed: false,
+		};
+		setTodos((prevTodos) => [newTodo, ...prevTodos]);
 	};
+
+	const toggleHandler = (id: number) => {
+		setTodos((prevTodos) =>
+			prevTodos.map((todo) => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			})
+		);
+	};
+
+	const removeHandler = (id: number) => {
+		setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+	};
+
 	return (
 		<div className={styles.app}>
 			<Navbar />
 			<TodoForm addTodo={addTodoHandler} />
+			<TodoList todos={todos} onToggle={toggleHandler} onRemove={removeHandler} />
 		</div>
 	);
 };
--- src/components/todosList/TodoList.tsx
+++ src/components/todosList/TodoList.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import cx from 'classnames';
+import styles from './TodoList.module.scss';
 
 type TodoListProps = {
 	todos: any[];
+	onToggle(id: number): void;
+	onRemove(id: number): void;
 };
 
-const todoList: React.FC<TodoListProps> = ({ todos }) => {
+const TodoList: React.FC<TodoListProps> = ({ todos, onToggle, onRemove }) => {
 	return (
-		<ul>
+		<ul className={styles.todoContainer}>
+			{todos.length === 0 && <h3>There are no todos to display</h3>}
 			{todos.map((todo) => (
-				<li>
-					<input type='checkbox' />
-					<span></span>
-					<i>delete</i>
+				<li className={styles.todo}>
+					<input type='checkbox' checked={todo.completed} onChange={onToggle.bind(null, todo.id)} />
+					<span className={todo.completed && styles.completed}>{todo.title}</span>
+					<i onClick={() => onRemove(todo.id)}>delete</i>
 				</li>
 			))}
 		</ul>
 	);
 };
 
-export default todoList;
+export default TodoList;
